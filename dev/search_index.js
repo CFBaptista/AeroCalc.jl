afe49f88036fe9b774d5ce6acb64f17468301843
Documenter.jl <documenter@juliadocs.github.io>
2024-06-03 01:54:24 +0000

    build based on bac3d36

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = AeroCalc","category":"page"},{"location":"#AeroCalc","page":"Home","title":"AeroCalc","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for AeroCalc.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [AeroCalc]","category":"page"}]
+}
